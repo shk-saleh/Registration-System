f0a2514f9874b913155b73ef938c6601803113fc
Muhammad Saleh <salehshazli786@gmail.com>
2025-04-19 00:00:04 +0500

    Bug fixed

--- Full Stack APP/Server/index.js
+++ Full Stack APP/Server/index.js
@@ -2,11 +2,14 @@ import express from "express"
 import cors from 'cors';
 import sql from "mysql2"
 
+// mandatory definitions
 const app = express();
 const router = express.Router();
 app.use(cors());
+app.use(express.json());
 
 app.use('/', router);
+
 
 const db = sql.createConnection({
 
@@ -18,20 +21,33 @@ const db = sql.createConnection({
 });
 
 
-// router.post('/', (req, res) => {
-//     const { firstname, lastname, email, password } = req.body;
-//     const q = "INSERT INTO registration VALUES (?, ?, ?, ?)";
-//     db.query(q, [firstname, lastname, email, password], (err, result) => {
-//       if (err) return res.json(err);
-//       return res.json("Student added!");
-//     });
-// });
-
-router.get('/', (req, res)=>{
-    res.send("hello");
+router.post('/', (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+    const q = "INSERT INTO registration VALUES (?, ?, ?, ?)";
+    db.query(q, [firstname, lastname, email, password], (err, result) => {
+      if (err) return res.json(err);
+      return res.json("Student added!");
+    });
 });
-  
 
+// const values = ["Muhammad", "Saleh", "saleh123@gmail.com", "123"];
+// const q = "INSERT INTO registration VALUES (?, ?, ?, ?)";
+
+// try{
+
+//     db.query(q, values, (err, result) => {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+
+// }catch(err){
+//     console.log(err);
+// }
+
+// router.get('/', (req, res)=>{
+//     res.send("hello hh");
+// });
+  
 app.listen(8000, () => {
   console.log('Server running on port 8000');
 });
